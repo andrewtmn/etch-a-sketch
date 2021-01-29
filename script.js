ee910d14f463abce08f7d23820af53153aea3d5e
Andrew Nguyen <andrewnguyenn229@gmail.com>
2021-01-29 18:30:51 +1000

    Fixed bug - childNodes was used as a function and not a property

--- script.js
+++ script.js
@@ -6,22 +6,24 @@ styleGrid();
 addGridHoverListener();
 
 function createGrid(numSideSquares) {
-    let grid = document.getElementById('sketch-grid');
+    let grid = document.getElementById("sketch-grid");
 
     for (let i = 0; i < numSideSquares**2; i++) {
-        let square = document.createElement('div');
+        let square = document.createElement("div");
         grid.appendChild(square);
     }
+
+    console.log(grid.childNodes);
 }
 
 function styleGrid() {
-    let grid = document.getElementById('sketch-grid');
-    let squares = grid.childNodes();
+    let grid = document.getElementById("sketch-grid");
+    let squares = grid.childNodes;
 
-    grid.classList.add('square-grid');
+    grid.classList.add("square-grid");
 
     squares.forEach((square) => {
-        square.classList.add('square');
+        square.classList.add("square");
     });
 }
 
