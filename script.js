91cde8459b73e0eb3de607357489715733085d96
Andrew Nguyen <andrewnguyenn229@gmail.com>
2021-01-29 18:18:35 +1000

    createGrid() and styleGrid() written to add square divs to the grid container and style it - code is not functional at this point

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+/* Create the sketch grid */
+createGrid(16);
+styleGrid();
+addGridHoverListener();
+
+function createGrid(numSideSquares) {
+    let grid = document.getElementById('sketch-grid');
+
+    for (let i = 0; i < numSideSquares**2; i++) {
+        let square = document.createElement('div');
+        grid.appendChild(square);
+    }
+}
+
+function styleGrid() {
+    let grid = document.getElementById('sketch-grid');
+    let squares = grid.childNodes();
+
+    grid.classList.add('square-grid');
+
+    squares.forEach((square) => {
+        square.classList.add('square');
+    });
+}
+
+function addGridHoverListener() {
+
+}
+
