e101743d40d60e8ec939d8dac1528fed5c95dfed
Andrew Nguyen <andrewnguyenn229@gmail.com>
2021-02-01 00:11:10 +1000

    fillSquare() function to fill square with random colour upon hover - implemented event handler setNewGrid() for Clear button

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 
 /* Create the sketch grid */
-createGrid(16);
+createGrid(50);
 styleGrid();
 addGridHoverListener();
+setupClearButton();
 
 function createGrid(numSideSquares) {
     let grid = document.getElementById("sketch-grid");
@@ -28,6 +29,35 @@ function styleGrid() {
 }
 
 function addGridHoverListener() {
+    let grid = document.getElementById("sketch-grid");
+    let squares = grid.childNodes;
 
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', fillSquare);
+    });
+}
+
+function fillSquare() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+
+function setupClearButton() {
+    let clearBtn = document.getElementById("clear-btn");
+    clearBtn.addEventListener('click', setNewGrid);
+}
+
+function setNewGrid() {
+    let grid = document.getElementById("sketch-grid");
+    grid.innerHTML = "";
+
+    let dim = prompt("How many squares long?");
+    dim = Number(dim);
+    createGrid(50);
+    styleGrid();
+    addGridHoverListener();
 }
 
